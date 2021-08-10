c355f0371df66b1934a3966894dde95ae3106f8d
@erick <erick.hx22@gmail.com>
2021-08-09 21:42:27 -0500

    refactor: replace imports css to scss (#1)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import '../styles/styles.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
