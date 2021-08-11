e6b603c9d4ae83376bac3941fc52fe8a94dec637
@erick <erick.hx22@gmail.com>
2021-08-10 22:00:10 -0500

    chore: implement vcontainer layout (#12)

--- layouts/VContainer.tsx
+++ layouts/VContainer.tsx
@@ -1,4 +1,7 @@
-const VContainer = () => {
-  return <div>this is a VContainer</div>;
-};
-export default VContainer;
+import { NextPage } from 'next'
+import styles from '../styles/layouts/VContainer.module.scss'
+
+const VContainer: NextPage = (props) => {
+  return <div className={styles.VContainer}>{props.children}</div>
+}
+export default VContainer
