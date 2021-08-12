9d871f43ea80639c353783b6598193d784667c0e
@erick <erick.hx22@gmail.com>
2021-08-12 09:58:29 -0500

    chore: impleemnt @svgr/webpack to next config (#22)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@ const path = require('path')
 
 module.exports = {
   reactStrictMode: true,
+
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config
+  }
 }
