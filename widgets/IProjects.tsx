4f9a48704b9b01a4d8cb1e2cc706d30fc0de0970
@erick <erick.hx22@gmail.com>
2021-10-14 18:42:27 -0500

    chore: implement widget for project section (#53)

---
+++ widgets/IProjects.tsx
@@ -0,0 +1,71 @@
+import Image from 'next/image'
+import { useTranslations } from 'next-intl'
+
+import Github from '../public/icons/github.svg'
+import ExternalLink from '../public/icons/external.svg'
+
+import styles from '../styles/widgets/IProjects.module.scss'
+
+const IProjects = () => {
+  const t = useTranslations('Index')
+
+  const project = {
+    name: 'Erick Escriba',
+    company: 'EquipIndustry',
+    companyWeb: 'http://equipindustry.com',
+    jobPosition: 'Front-end Developer',
+    email: 'erickescribaa@gmail.com',
+    techList: ['VS Code', 'Atom', 'Atom'],
+    links: [
+      { icon: Github, url: 'https://github.com/Aireck2' },
+      { icon: ExternalLink, url: 'https://codepen.io/Aireck' },
+    ],
+  }
+  return (
+    <div className={styles.IProjects__container}>
+      <h2 className={styles.IProjects__heading}>{t('projectsTitle')}</h2>
+
+      <li className={styles.IProjects__inner__container}>
+        <div className={styles.IProjects__inner__content}>
+          <div>
+            <div className={styles.IProjects__inner__content__description}>
+              <p className={styles.IProjects__subheading__overline}>{`Featured Project`}</p>
+              <p className={styles.IProjects__subheading__title}>{`Halcyon Theme`}</p>
+              <p className={styles.IProjects__body}>
+                {`Halcyon Theme Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Aut non aliquid nesciunt repudiandae? Ad repellendus, blanditiis perspiciatis,
+              nisi eveniet aspernatur accusantium ipsa adipisci delectus`}
+              </p>
+              <ul className={styles.IProjects__inner__content__list}>
+                {project.techList.map((tech, index) => (
+                  <li key={index}>{tech}</li>
+                ))}
+              </ul>
+              <ul className={styles.IProjects__inner__content__links}>
+                {project.links.map((link, index) => (
+                  <li key={index}>
+                    <a href={`${link.url}`} target="_blank" rel="noreferrer">
+                      <link.icon />
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className={styles.IProjects__inner__image}>
+          <a href="">
+            <Image
+              className="project-img"
+              src="/images/halcyon.png"
+              alt="Project photo"
+              width={700}
+              height={438}
+            />
+          </a>
+        </div>
+      </li>
+    </div>
+  )
+}
+export default IProjects
--- widgets/index.ts
+++ widgets/index.ts
@@ -4,5 +4,6 @@ import IExperience from './IExperience'
 import IContact from './IContact'
 import IEmail from './IEmail'
 import ISocial from './ISocial'
+import IProjects from './IProjects'
 
-export { IHome, IAbout, IExperience, IContact, IEmail, ISocial }
+export { IHome, IAbout, IExperience, IContact, IEmail, ISocial, IProjects }
