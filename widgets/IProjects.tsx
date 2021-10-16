26f22cdd3425cdd30e6623b0afb124e94cb3f104
@erick <erick.hx22@gmail.com>
2021-10-16 15:28:02 -0500

    chore: implement method to map projects (#57)

--- widgets/IProjects.tsx
+++ widgets/IProjects.tsx
@@ -9,62 +9,78 @@ import styles from '../styles/widgets/IProjects.module.scss'
 const IProjects = () => {
   const t = useTranslations('Index')
 
-  const project = {
-    name: 'Erick Escriba',
-    company: 'EquipIndustry',
-    companyWeb: 'http://equipindustry.com',
-    jobPosition: 'Front-end Developer',
-    email: 'erickescribaa@gmail.com',
-    techList: ['VS Code', 'Atom', 'Atom'],
-    links: [
-      { icon: Github, url: 'https://github.com/Aireck2' },
-      { icon: ExternalLink, url: 'https://codepen.io/Aireck' },
-    ],
-  }
+  const projects = [
+    {
+      name: 'Halcyon Theme',
+      description: `Halcyon Theme Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Aut non aliquid nesciunt repudiandae? Ad repellendus, blanditiis perspiciatis,
+              nisi eveniet aspernatur accusantium ipsa adipisci delectus`,
+      technologies: ['VS Code', 'Atom', 'Atom'],
+      links: [
+        { icon: Github, url: 'https://github.com/Aireck2' },
+        { icon: ExternalLink, url: 'https://codepen.io/Aireck' },
+      ],
+    },
+    {
+      name: 'Halcyon Theme',
+      description: `Halcyon Theme Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Aut non aliquid nesciunt repudiandae? Ad repellendus, blanditiis perspiciatis,
+              nisi eveniet aspernatur accusantium ipsa adipisci delectus`,
+      technologies: ['VS Code', 'Atom', 'Atom'],
+      links: [
+        { icon: Github, url: 'https://github.com/Aireck2' },
+        { icon: ExternalLink, url: 'https://codepen.io/Aireck' },
+      ],
+    },
+  ]
   return (
     <div className={styles.IProjects__container}>
       <h2 className={styles.IProjects__heading}>{t('projectsTitle')}</h2>
 
-      <li className={styles.IProjects__inner__container}>
-        <div className={styles.IProjects__inner__content}>
-          <div>
-            <div className={styles.IProjects__inner__content__description}>
-              <p className={styles.IProjects__subheading__overline}>{`Featured Project`}</p>
-              <p className={styles.IProjects__subheading__title}>{`Halcyon Theme`}</p>
-              <p className={styles.IProjects__body}>
-                {`Halcyon Theme Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      <ul>
+        {projects.map((project, index) => (
+          <li className={styles.IProjects__inner__container} key={index}>
+            <div className={styles.IProjects__inner__content}>
+              <div>
+                <div className={styles.IProjects__inner__content__description}>
+                  <p className={styles.IProjects__subheading__overline}>Featured Project</p>
+                  <p className={styles.IProjects__subheading__title}>{`Halcyon Theme`}</p>
+                  <p className={styles.IProjects__body}>
+                    {`Halcyon Theme Lorem ipsum dolor sit amet consectetur adipisicing elit. 
               Aut non aliquid nesciunt repudiandae? Ad repellendus, blanditiis perspiciatis,
               nisi eveniet aspernatur accusantium ipsa adipisci delectus`}
-              </p>
-              <ul className={styles.IProjects__inner__content__list}>
-                {project.techList.map((tech, index) => (
-                  <li key={index}>{tech}</li>
-                ))}
-              </ul>
-              <ul className={styles.IProjects__inner__content__links}>
-                {project.links.map((link, index) => (
-                  <li key={index}>
-                    <a href={`${link.url}`} target="_blank" rel="noreferrer">
-                      <link.icon />
-                    </a>
-                  </li>
-                ))}
-              </ul>
+                  </p>
+                  <ul className={styles.IProjects__inner__content__list}>
+                    {project.technologies.map((tech, index) => (
+                      <li key={index}>{tech}</li>
+                    ))}
+                  </ul>
+                  <ul className={styles.IProjects__inner__content__links}>
+                    {project.links.map((link, index) => (
+                      <li key={index}>
+                        <a href={`${link.url}`} target="_blank" rel="noreferrer">
+                          <link.icon />
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className={styles.IProjects__inner__image}>
-          <a href="">
-            <Image
-              className="project-img"
-              src="/images/halcyon.png"
-              alt="Project photo"
-              width={700}
-              height={438}
-            />
-          </a>
-        </div>
-      </li>
+            <div className={styles.IProjects__inner__image}>
+              <a href="">
+                <Image
+                  className="project-img"
+                  src="/images/halcyon.png"
+                  alt="Project photo"
+                  width={700}
+                  height={438}
+                />
+              </a>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
