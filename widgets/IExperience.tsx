2994493603a411b0ccab6587ca993037927a8ebc
@erick <erick.hx22@gmail.com>
2021-11-01 18:12:11 -0500

    feat: implement styles to about section (#65)

--- widgets/IExperience.tsx
+++ widgets/IExperience.tsx
@@ -1,16 +1,16 @@
 import { useTranslations } from 'next-intl'
 
+import QList from '@components/QList'
+
 import styles from '@styles/widgets/IExperience.module.scss'
 
 const IExperience = () => {
   const t = useTranslations('Index')
 
   const info = {
-    name: 'Erick Escriba',
     company: 'EquipIndustry',
     companyWeb: 'http://equipindustry.com',
     jobPosition: 'Front-end Developer',
-    email: 'erickescribaa@gmail.com',
     jobDescription: [
       t('jobDescription1'),
       t('jobDescription2'),
@@ -31,13 +31,8 @@ const IExperience = () => {
         </span>
       </p>
       <p className={styles.IExperience__body}>{t('jobDate1')}</p>
-      <ul className={styles.IExperience__skill_list}>
-        {info.jobDescription.map((job, index) => (
-          <li key={index} className={styles.IExperience__skill_item}>
-            {job}
-          </li>
-        ))}
-      </ul>
+
+      <QList arr={info.jobDescription} />
     </div>
   )
 }
