242f096341e03b927a87924928d798e1e05380e9
@erick <erick.hx22@gmail.com>
2021-11-03 22:08:04 -0500

    refactor: implement particles bg to home (#64)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 
+import Particles from 'react-tsparticles'
+
 import { VContainer, VMenu } from '@layouts/index'
 import {
   IAbout,
@@ -26,37 +28,117 @@ const Home: NextPage = (_props) => {
         <meta name="keywords" content={'Erick EA'} />
         <link rel="icon" href="/logos/logo.svg" />
       </Head>
-      <VMenu />
-      <IEmail />
-      <ISocial />
-      <div className={styles.Home__banner}>
-        <VContainer>
-          <IHome />
-        </VContainer>
-      </div>
+      <header>
+        <VMenu />
+      </header>
+      <main>
+        <IEmail />
+        <ISocial />
 
-      <div className={styles.Home__about} id="about">
-        <VContainer>
-          <IAbout />
-        </VContainer>
-      </div>
-      <div className={styles.Home__experience} id="experience">
-        <VContainer>
-          <IExperience />
-        </VContainer>
-      </div>
-      <div className={styles.Home__experience} id="projects">
-        <VContainer>
-          <IProjects />
-          <IOtherProjects />
-        </VContainer>
-      </div>
+        <section className={styles.Home__banner}>
+          <VContainer>
+            <IHome />
+          </VContainer>
+        </section>
 
-      <div className={styles.Home__contact} id="contact">
-        <VContainer>
-          <IContact />
-        </VContainer>
-      </div>
+        <section className={styles.Home__about} id="about">
+          <VContainer>
+            <IAbout />
+          </VContainer>
+        </section>
+        <div className={styles.Home__experience} id="experience">
+          <VContainer>
+            <IExperience />
+          </VContainer>
+        </div>
+        <section className={styles.Home__experience} id="projects">
+          <VContainer>
+            <IProjects />
+            <IOtherProjects />
+          </VContainer>
+        </section>
+
+        <section className={styles.Home__contact} id="contact">
+          <VContainer>
+            <IContact />
+          </VContainer>
+        </section>
+      </main>
+      <Particles
+        id="tsparticles"
+        options={{
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'bubble',
+              },
+              onHover: {
+                enable: true,
+                mode: 'grab',
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 20,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#1d1e1f',
+            },
+            links: {
+              color: '#bfbfc0',
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outMode: 'bounce',
+              random: false,
+              speed: 0.5,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 30,
+            },
+            opacity: {
+              value: 0.2,
+            },
+            shape: {
+              type: 'round',
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
     </>
   )
 }
