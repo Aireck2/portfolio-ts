26d0a6d2d4dec9641ebc8a004f7ecc0497239bc1
@erick <erick.hx22@gmail.com>
2021-11-06 11:48:45 -0500

    chore: implement qbutton component (#67)

--- components/QButton.tsx
+++ components/QButton.tsx
@@ -1,4 +1,16 @@
-const QButton = () => {
-  return <div>this is a QButton</div>;
-};
-export default QButton;
+import styles from '@styles/components/QButton.module.scss'
+interface IProps {
+  href: string
+  text: string
+}
+const QButton: React.FC<IProps> = (props) => {
+  const { href, text } = props
+  return (
+    <button className={styles.QButton}>
+      <a href={href} target="_blank" rel="noreferrer">
+        {text}
+      </a>
+    </button>
+  )
+}
+export default QButton
