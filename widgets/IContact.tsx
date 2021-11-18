48ee2bf2bb31357a09e00db6ad64349567fda39d
@erick <erick.hx22@gmail.com>
2021-11-17 21:29:34 -0500

    chore: implement data fetching from repositoy (#79)

--- widgets/IContact.tsx
+++ widgets/IContact.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { useTranslations } from 'next-intl'
 
 import { QButton, QIconLink } from '@components/index'
@@ -6,15 +7,44 @@ import styles from '@styles/widgets/IContact.module.scss'
 
 const IContact = () => {
   const t = useTranslations('Index')
+  const [githubInfo, setGitHubInfo] = useState({
+    stars: '0',
+    forks: '0',
+  })
 
   const info = {
-    githubRepo: 'https://github.com/Aireck2/portfolio-next',
     email: 'erickescribaa@gmail.com',
     github: [
-      { type: 'star' as const, url: 'https://github.com/Aireck2/portfolio-next' },
-      { type: 'fork' as const, url: 'https://github.com/Aireck2/portfolio-next' },
+      {
+        type: 'star' as const,
+        url: 'https://github.com/Aireck2/portfolio-next',
+        count: githubInfo.stars,
+      },
+      {
+        type: 'fork' as const,
+        url: 'https://github.com/Aireck2/portfolio-next',
+        count: githubInfo.forks,
+      },
+    ],
+    socialArr: [
+      { type: 'github' as const, url: 'https://github.com/Aireck2' },
+      { type: 'instagram' as const, url: 'https://www.instagram.com/_aireck._/' },
+      { type: 'linkedIn' as const, url: 'https://www.linkedin.com/in/erickescriba/' },
+      { type: 'codePen' as const, url: 'https://codepen.io/Aireck' },
     ],
   }
+  useEffect(() => {
+    fetch('https://api.github.com/repos/Aireck2/Aireck2')
+      .then((response) => response.json())
+      .then((json) => {
+        const { stargazers_count, forks_count } = json
+        setGitHubInfo({
+          stars: stargazers_count,
+          forks: forks_count,
+        })
+      })
+      .catch((e) => console.error(e))
+  }, [])
 
   return (
     <div className={styles.IContact__container}>
@@ -25,10 +55,18 @@ const IContact = () => {
 
         <QButton href={`mailto:${info.email}`} text={t('sayHello')} />
       </div>
+
+      <ul className={styles.IContact__social}>
+        {info.socialArr.map((social, index) => (
+          <li key={index}>
+            <QIconLink type={social.type} url={social.url} />
+          </li>
+        ))}
+      </ul>
       <ul className={styles.IContact__github_stats}>
         {info.github.map((item, index) => (
           <li key={index}>
-            <QIconLink type={item.type} url={item.url} size="small" text="3" />
+            <QIconLink type={item.type} url={item.url} size="small" text={item.count.toString()} />
           </li>
         ))}
       </ul>
