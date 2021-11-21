24a3733b68e74bff636744fbda5d78b547cfda67
@erick <erick.hx22@gmail.com>
2021-11-21 12:59:10 -0500

    chore: implement list for main projects (#82)

--- widgets/IProjects/IProjects.tsx
+++ widgets/IProjects/IProjects.tsx
@@ -1,37 +1,14 @@
 import Image from 'next/image'
 import { useTranslations } from 'next-intl'
 
-import QIconLink from '@components/QIconLink'
+import { QIconLink } from '@components/index'
+import { projects } from './data'
 
 import styles from '@styles/widgets/IProjects.module.scss'
 
 const IProjects = () => {
   const t = useTranslations('Index')
 
-  const projects = [
-    {
-      name: 'Halcyon Theme',
-      description: `Halcyon Theme Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Aut non aliquid nesciunt repudiandae? Ad repellendus, blanditiis perspiciatis,
-              nisi eveniet aspernatur accusantium ipsa adipisci delectus`,
-      technologies: ['VS Code', 'Atom', 'Atom'],
-      links: [
-        { type: 'github' as const, url: 'https://github.com/Aireck2' },
-        { type: 'externalLink' as const, url: 'https://codepen.io/Aireck' },
-      ],
-    },
-    {
-      name: 'Halcyon Theme',
-      description: `Halcyon Theme Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Aut non aliquid nesciunt repudiandae? Ad repellendus, blanditiis perspiciatis,
-              nisi eveniet aspernatur accusantium ipsa adipisci delectus`,
-      technologies: ['VS Code', 'Atom', 'Atom'],
-      links: [
-        { type: 'github' as const, url: 'https://github.com/Aireck2' },
-        { type: 'externalLink' as const, url: 'https://codepen.io/Aireck' },
-      ],
-    },
-  ]
   return (
     <div className={styles.IProjects__container}>
       <h2 className={styles.IProjects__heading}>{t('projectsTitle')}</h2>
@@ -44,13 +21,9 @@ const IProjects = () => {
                 <div className={styles.IProjects__inner__content__description}>
                   <div className={styles.IProjects__subheading__container}>
                     <p className={styles.IProjects__subheading__overline}>Featured Project</p>
-                    <p className={styles.IProjects__subheading__title}>{`Halcyon Theme`}</p>
+                    <p className={styles.IProjects__subheading__title}>{project.name}</p>
                   </div>
-                  <p className={styles.IProjects__body}>
-                    {`Halcyon Theme Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Aut non aliquid nesciunt repudiandae? Ad repellendus, blanditiis perspiciatis,
-              nisi eveniet aspernatur accusantium ipsa adipisci delectus`}
-                  </p>
+                  <p className={styles.IProjects__body}>{project.description}</p>
                   <ul className={styles.IProjects__inner__content__list}>
                     {project.technologies.map((tech, index) => (
                       <li key={index}>{tech}</li>
@@ -67,10 +40,10 @@ const IProjects = () => {
               </div>
             </div>
             <div className={styles.IProjects__inner__image}>
-              <a href="">
+              <a href={project.imageUrl} target="_blank" rel="noreferrer">
                 <Image
                   className="project-img"
-                  src="/images/halcyon.png"
+                  src={project.imageRef}
                   priority={true}
                   alt="Project photo"
                   width={700}
