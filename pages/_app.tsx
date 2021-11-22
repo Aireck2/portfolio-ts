4a039edb5cea4a13a8062cdd303a3101176af072
@erick <erick.hx22@gmail.com>
2021-11-21 20:06:07 -0500

    chore: implement google analytics config (#89)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+
 import { NextIntlProvider } from 'next-intl'
+
+import gtag from 'helpers/ga.helper'
+import config from 'helpers/config.helper'
 
 import '@styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageView(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <NextIntlProvider
       // To achieve consistent date, time and number formatting
@@ -26,6 +45,25 @@ function MyApp({ Component, pageProps }: AppProps) {
       // in a different time zone.
       timeZone="Central Daylight Time"
     >
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${config.marketing.ga.trackingId}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           
+           gtag('config', '${config.marketing.ga.trackingId}',{
+             page_path: window.location.pathname,
+           });
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </NextIntlProvider>
   )
