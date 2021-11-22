e9d044f1abe1bc8fe4de01e3915657fd67b843db
@erick <erick.hx22@gmail.com>
2021-11-21 20:05:24 -0500

    feat: implement methods to ga helper (#89)

---
+++ helpers/ga.helper.ts
@@ -0,0 +1,23 @@
+import config from './config.helper'
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageView = (url: string) => {
+  window.gtag('config', config.marketing.ga.trackingId, {
+    page_path: url,
+  })
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: any) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
+}
+const gtag = {
+  pageView,
+  event,
+}
+
+export default gtag
