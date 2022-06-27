921dd13bd5747c4ee819f7de6529ad3db532b6a8
@erick <erick.hx22@gmail.com>
2022-06-27 16:36:59 -0500

    chore: implement languages dropdown (#93)

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -3,19 +3,28 @@ import { NextPage } from 'next'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+
+import { Dropdown, useTheme } from '@nextui-org/react'
 import { useTranslations } from 'next-intl'
+import { useTheme as useNextTheme } from 'next-themes'
+
+import constants, { Languages } from '@helpers/constants'
 
 import styles from '@styles/layouts/VMenu.module.scss'
 
 const Navbar: NextPage = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const { locale, locales, route } = useRouter()
+  const { setTheme } = useNextTheme()
+  const { isDark } = useTheme()
 
   const t = useTranslations('Index')
 
-  const otherLocale = locales?.find((cur) => cur !== locale)
+  const otherLocale = locales?.filter((cur) => cur !== locale)
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
+
+  const toggleTheme = () => setTheme(isDark ? 'light' : 'dark')
 
   const menuItems = [
     { name: t('about'), url: '#about' },
@@ -32,7 +41,6 @@ const Navbar: NextPage = () => {
             <a href={item.url}>{item.name}</a>
           </li>
         ))}
-
         <li key="button">
           <button className={styles.VMenu__button}>
             <Link href={`/resume_${locale?.toUpperCase()}.pdf`}>
@@ -42,24 +50,40 @@ const Navbar: NextPage = () => {
             </Link>
           </button>
         </li>
-        <li key="button-locale">
-          <Link href={route} locale={otherLocale}>
-            <a>
-              <button className={styles.VMenu__button}>
-                {locale === 'es' ? (
-                  <>
-                    <Image src="/images/flag_spain.png" alt="" width="23.3" height="15.6" />
-                    <span style={{ marginLeft: 8 }}>Español</span>
-                  </>
-                ) : (
-                  <>
-                    <Image src="/images/flag_usa.png" alt="" width="23.3" height="15.6" />
-                    <span style={{ marginLeft: 8 }}>English</span>
-                  </>
-                )}
-              </button>
-            </a>
-          </Link>
+        <li style={{ display: 'flex', gridGap: 5 }}>
+          <Dropdown>
+            <Dropdown.Button
+              flat
+              color="primary"
+              icon={constants.languages[locale as Languages].icon}
+            >
+              {constants.languages[locale as Languages].text}
+            </Dropdown.Button>
+
+            <Dropdown.Menu aria-label="Static Actions" disabledKeys={['de']}>
+              {otherLocale?.map((locale) => (
+                <Dropdown.Item
+                  key={locale}
+                  css={{ a: { display: 'block' } }}
+                  icon={constants.languages[locale as Languages].icon}
+                  textValue={locale}
+                >
+                  {locale !== 'de' ? (
+                    <Link href={route} locale={locale}>
+                      <a>{constants.languages[locale as Languages].text}</a>
+                    </Link>
+                  ) : (
+                    constants.languages[locale as Languages].text
+                  )}
+                </Dropdown.Item>
+              )) ?? (
+                <Dropdown.Item key={'es'} textValue={'ES'}>
+                  ES
+                </Dropdown.Item>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+          {/* <Button auto flat onClick={toggleTheme} icon={isDark ? <Sun /> : <Moon />} /> */}
         </li>
       </ul>
     </nav>
