e2b626fb5d55f2ac03a14362d445a075ac638d5b
@erick <erick.hx22@gmail.com>
2022-07-12 16:26:27 -0500

    (#103)

--- components/basics/Button.tsx
+++ components/basics/Button.tsx
@@ -1,3 +1,4 @@
+import { styled, Button as NButton } from '@nextui-org/react'
 import styles from '@styles/components/QButton.module.scss'
 
 interface IProps {
@@ -5,13 +6,56 @@ interface IProps {
   text: string
 }
 
-export const Button: React.FC<IProps> = (props) => {
-  const { href, text } = props
+const StyledButton = styled('div', {
+  display: 'inline-block',
+  position: 'relative',
+  zIndex: 1,
+  padding: '13px 0',
+  a: {
+    position: 'relative',
+    padding: '13px 30px',
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    backgroundColor: '$background',
+    color: '$accents10',
+    fontSize: 18,
+    fontFamily: '$mono',
+    '&::after': {
+      content: '',
+      position: 'absolute',
+      zIndex: -1,
+      top: -2,
+      left: -2,
+      height: 'calc(100% + 4px)',
+      width: 'calc(100% + 4px)',
+      background: 'linear-gradient(90deg, #f7b500, #b620e0, #32c5ff)',
+      borderRadius: 12,
+      transition: 'all 300ms ease',
+      // opacity: 0.7,
+    },
+  },
+  '&:hover a': {
+    '&::after': {
+      filter: 'blur(3px)',
+      opacity: 1,
+    },
+  },
+})
+
+export const Button: React.FC<IProps> = ({ href, text }) => {
+  // return (
+  //   <button className={styles.QButton}>
+  //     <a href={href} target="_blank" rel="noreferrer">
+  //       {text}
+  //     </a>
+  //   </button>
+  // )
   return (
-    <button className={styles.QButton}>
+    <StyledButton>
       <a href={href} target="_blank" rel="noreferrer">
         {text}
       </a>
-    </button>
+    </StyledButton>
   )
 }
